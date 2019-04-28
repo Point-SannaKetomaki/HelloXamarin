3d331adb08f66d87c480e28d1e4f585a63d96390
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-04-28 14:49:43 +0300

    Lisätty käyttöliittymään Klikkaa mua! -nappi ja sille toiminnallisuus

---
+++ HelloXamarin/HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int laske = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            laske++;
+            ((Button)sender).Text = $"Klikkasit {laske} kertaa.";
+        }
+
+
+    }
+}
